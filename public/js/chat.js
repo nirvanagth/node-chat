31dbd4c85fe0b18e2f2e8cbc16b0d5fe9822644e
Tianhao Gu <nirvanagth@gmail.com>
2017-11-07 20:54:31 +0800

    Add users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,22 +17,42 @@ function scrollToBottom() {
 }
 socket.on('connect', function () { //client side js should use function in case browser does not support ES6
     console.log('Connected to server')
+    var params = jQuery.deparam(window.location.search)
 
+    socket.emit('join', params, function (err) {
+        if (err) {
+            alert(err)
+            window.location.href = "/"
+        } else {
+            console.log('No error')
+        }
+
+    })
 
     // socket.emit('createMessage', {
     //     from: 'Tianhao',
     //     text: 'shit happens'
     // })
-    socket.emit('createEmail', {
-        to: 'jen@example.com',
-        text: 'Hey. This is Tianhao'
-    })
+    // socket.emit('createEmail', {
+    //     to: 'jen@example.com',
+    //     text: 'Hey. This is Tianhao'
+    // })
 })
 
 socket.on('disconnect', function () {
     console.log('Disconnected from server')
 })
 
+
+socket.on('updateUserList', function (users) {
+    var ol = jQuery('<ol></ol>')
+
+    users.forEach(function (user) {
+        ol.append(jQuery('<li></li>').text(user))
+    })
+
+    jQuery('#users').html(ol)
+})
 socket.on('newEmail', function (email) {
     console.log('New Email', email)
 })
--- server/server.js
+++ server/server.js
@@ -5,22 +5,21 @@ const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public')
 const {generateMessage, generateLocationMessage} = require('./utils/message')
+const {isRealString} = require('./utils/validation')
+const {Users} = require('./utils/users')
 const port = process.env.PORT || 3000
 
 var app = express()
 var server = http.createServer((app))
 var io = socketIO(server)
+var users = new Users()
 
 app.use(express.static(publicPath)) //config static express middleware
 
 io.on('connection', (socket) => {
     console.log('New user connected')
 
-    // socket.emit from Admin text Welcome to the chat app
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
 
-    // socket.broadcast.emit from Admin text New usr joined
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
     socket.emit('newEmail', {
         from: 'tianhao@gmail.com',
@@ -38,6 +37,25 @@ io.on('connection', (socket) => {
     socket.on('createEmail', (newEmail) => {
         console.log('createEmail', newEmail)
     })
+
+    socket.on('join', (params, callback) => {
+        if (!isRealString(params.name) || !isRealString(params.room)) {
+            return callback('Name and room number are required')
+        }
+
+        socket.join(params.room) //let people talk in the same room
+        users.removeUser(socket.id)
+        users.addUser(socket.id, params.name, params.room)
+
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room))
+        // socket.emit from Admin text Welcome to the chat app
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
+
+        // socket.broadcast.emit from Admin text New usr joined
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`))
+
+        callback()
+    })
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message)
         io.emit('newMessage', generateMessage(message.from, message.text))// send the message to all users including self
@@ -54,7 +72,11 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
-        console.log('User disconnected')
+        var user = users.removeUser(socket.id)
+        if (user) {
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room))
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`))
+        }
     })
 })
 
